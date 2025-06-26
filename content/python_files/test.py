8715b66d4b27d5848c4c22c1ee55471cbac19587
Olivier Grisel <olivier.grisel@ensta.org>
2025-06-26 14:34:34 +0200

    Install skrub dev wheel from a dedicated anaconda index

--- content/python_files/test.py
+++ content/python_files/test.py
@@ -8,6 +8,16 @@ import sklearn
 sklearn.__version__
 
 # %%
+%pip install https://github.com/skrub-data/skrub@771f3870a23438211faefb0e54132781256acc0a
+
+# %% [markdown]
+# Install the development version of skrub to be able to use the
+# skrub expressions.
+
+# %%
+%pip -q install https://pypi.anaconda.org/ogrisel/simple/skrub/0.6.dev0/skrub-0.6.dev0-py3-none-any.whl
+
+# %%
 import skrub
 
 skrub.__version__
