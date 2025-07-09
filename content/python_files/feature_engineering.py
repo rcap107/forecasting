e6e65d2d6234ff4529b77ca58e7b4c717422f80f
Olivier Grisel <olivier.grisel@ensta.org>
2025-07-09 13:57:50 +0200

    Intro paragraph

--- content/python_files/feature_engineering.py
+++ content/python_files/feature_engineering.py
@@ -18,11 +18,12 @@
 # holidays and calendar features are known at prediction time for any point in
 # the future.
 #
-# Therefore, features derived from the weather and calendar data can be used to
-# engineer "future covariates". Since the load data is our prediction target,
-# we will can also use it to engineer "past covariates" such as lagged features
-# and rolling aggregations. The future values of the load data (with respect to
-# the prediction time) are used as targets for the forecasting model.
+# Therefore, exogenous features derived from the weather and calendar data can
+# be used to engineer "future covariates". Since the load data is our
+# prediction target, we will can also use it to engineer "past covariates" such
+# as lagged features and rolling aggregations. The future values of the load
+# data (with respect to the prediction time) are used as targets for the
+# forecasting model.
 #
 # ## Environment setup
 #
